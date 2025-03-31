e6a8d4f3ec3a171322d92fe489c8060e421fa6f3
João Geraldo Bussolo Bevilacqua <joaogeraldobussolo@gmail.com>
2025-03-31 10:12:36 -0300

    31/03

--- javascript/exercicios estrutura de decisao/funcoes.js
+++ javascript/exercicios estrutura de decisao/funcoes.js
@@ -11,3 +11,24 @@ function ex1(){
 
 
 }
+
+
+function ex2(){
+
+    let n1 = Number(document.getElementById("n1").value);
+    let n2 = Number(document.getElementById("n2").value);
+    let n3 = Number(document.getElementById("n3").value);
+
+    let soma = n1+n2;
+
+    if(soma > n3){
+        document.getElementById("C").innerHTML = "C vale: " + n3;
+        document.getElementById("resp").innerHTML = "A soma de A e B é maior q C." + soma;
+        
+    }else{
+        document.getElementById("C").innerHTML = "C vale: " + n3;
+        document.getElementById("resp").innerHTML="C é maior que a soma entre A e B." + soma;
+    }
+    
+
+}
